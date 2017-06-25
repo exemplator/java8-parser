78ed951fd1a780911cf39d02cb11270b7312cd0a
LeanderK <Leander.Kurscheidt@gmx.de>
2017-06-25 18:40:15 +0200

    started working on the Lib

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# exemplator-client
+# java 8 parser
+
+This project finds occurences of methods, classes and packages in java-files.
+
+There are two stack-projects, stack.yaml is used for development and has only the lib-module. Just run `stack setup` and `stack install` to use it. Stack-GHCJS.yaml uses the ghcjs compiler and compiles to javascript. The interface to interact with Javascript is defined in the client module. Prepend the commands with `STACK_YAML=stack-GHCJS.yaml`, then run `stack setup` and `stack install`. If `stack install` fails you have to run `stack build alex`first.
--- lib/src/Lib.hs
+++ lib/src/Lib.hs
@@ -4,12 +4,40 @@ module Lib
 
 import Language.Java.Parser
 
+data Input = String Command
+
+data Command = Command {
+    packageName :: Maybe String
+    , className :: Maybe String
+    , methodName :: Maybe String
+    } deriving (Show)
+
+data Position = Position {
+    line :: Integer
+    , column :: Integer
+    } deriving (Show)
+
+data Selection = Selection {
+    start :: Position,
+    end :: Position
+    } deriving (Show)
+
+getSelections :: Input -> Either String [Selection]
+getSelections input = undefined
+
+correctPackage :: String -> Command -> Bool
+correctPackage package command
+    | 
+
+
 parse :: String -> String
 parse input = toString (result input)
     where 
         toString (Right _) = "parsed successfully"
         toString _ = "error while parsing"
         result input = parser compilationUnit input
+
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
