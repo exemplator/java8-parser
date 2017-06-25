93d5210b0ca1deb430a7415a2dc8edaa72741f3d
LeanderK <Leander.Kurscheidt@gmx.de>
2017-06-25 20:25:15 +0200

    made it compile again

--- lib/src/Usage.hs
+++ lib/src/Usage.hs
@@ -8,8 +8,8 @@ import           Language.Java.Syntax
 data Import = StaticImp | NormalImp
 
 
-isOriginal :: PackageDecl -> String
-isOriginal (PackageDecl Name) = 
+isOriginal :: PackageDecl -> String -> Bool
+isOriginal (PackageDecl name) package = splitPackage package == nameToString name
 
 isImported :: ImportDecl -> String -> Maybe Import
 isImported (ImportDecl static importName generic) package
@@ -20,8 +20,11 @@ isImported (ImportDecl static importName generic) package
         checkImported True = iName `isPrefixOf` splittedPackages
         checkImported False = iName == splittedPackages
         iName = nameToString importName
-        splittedPackages = splitOn "." package
+        splittedPackages = splitPackage package
         correctImport = if static then StaticImp else NormalImp
 
 nameToString :: Name -> [String]
 nameToString (Name idents) = map (\(Ident x) -> x) idents
+
+splitPackage :: String -> [String]
+splitPackage = splitOn "."
