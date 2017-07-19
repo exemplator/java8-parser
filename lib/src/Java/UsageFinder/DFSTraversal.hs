5c5e2b13fcdf544e94dc2be9d5291d17a47f0a1b
LeanderK <Leander.Kurscheidt@gmx.de>
2017-07-19 17:01:58 +0200

    forgot to save

--- lib/src/Java/UsageFinder/DFSTraversal.hs
+++ lib/src/Java/UsageFinder/DFSTraversal.hs
@@ -57,31 +57,3 @@ traverseTypeDecl isOriginalPackage (target, typeSource) typeDecl = next
 
 traverseBody :: SearchBehaviour -> TypeDecl l-> [Result l]
 traverseBody = undefined
-
-
-
-
-
-isImported :: Target -> CompilationUnit l -> [Result l]
-isImported target (CompilationUnit l pkgDcl importDecls tDcl) = importDecls >>= scanImported (targetType target)
-    where
-        scanImported :: TargetType -> ImportDecl l -> [Result l]
-        scanImported targetType (ImportDecl l True pkg) = if targetType == RelaxedType (getType pkg)
-            then isOrignal target TypeSource {inPackageScope = False, inClassScope = True} [RImportDecl (ImportDecl l True pkg)] (CompilationUnit l pkgDcl importDecls tDcl)   -- A static import is matched
-            else isOrignal target TypeSource {inPackageScope = False, inClassScope = False} [] (CompilationUnit l pkgDcl importDecls tDcl)                                                                                                       -- No static is no static import
-        scanImported targetType (ImportDecl l False pkg) = if targetType == RelaxedType (getType pkg)
-            then isOrignalWithImport target [RImportDecl (ImportDecl l True pkg)] (CompilationUnit l pkgDcl importDecls tDcl)        -- An import is matched
-            else isOrignalWithImport target [] (CompilationUnit l pkgDcl importDecls tDcl)                                           -- No import is matched
-
-isOrignal ::  Target -> TypeSource -> CompilationUnit l -> [Result l]
-isOrignal = undefined
-
-isOriginalPackage :: TargetType -> PackageDecl l -> TypeDecl l -> [ImportDecl l]
-isOriginalPackage = undefined
-
-parse :: Target -> [Result l] -> CompilationUnit l -> [Result l]
-parseWithImported = undefined
-
-parse :: Target -> [Result l] -> CompilationUnit l -> [Result l]
-parseWithoutImported = undefined
-
